be800628377fbbe94ae2ff227b272f118aa45c92
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-12-12 13:26:18 +0100

    Lop4j2 and JUnit BOM

---
+++ src/main/java/de/schulung/samples/Start.java
@@ -0,0 +1,13 @@
+package de.schulung.samples;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Start {
+
+    private static final Logger logger = LogManager.getLogger(Start.class);
+
+    public static void main(String[] args) {
+        logger.info("Anwendung startet.");
+    }
+
+}
