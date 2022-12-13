628d747256d5dd5cec42c27814b060e07babcf99
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-12-13 15:20:24 +0100

    Switch to multi module

---
+++ maven-sample2/src/main/java/de/schulung/samples/StartWithoutLogger.java
@@ -0,0 +1,10 @@
+package de.schulung.samples;
+
+public class StartWithoutLogger {
+
+    public static void main(String[] args) {
+        System.out.println("Anwendung startet.");
+        System.out.println(new Person("Tom").sayHello());
+    }
+
+}
